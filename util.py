af793616736a2bad8f8c19db97156e32c9c47ddb
Xan Mead <x.m.mead@gmail.com>
2017-07-21 13:17:51 -0400

    added functions for triangular sampling; may benefit from optimization

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
-from random import randint
+from random import randint, triangular
+from math import floor
 
 def roll(dice):
     """Takes arguments in the format "{#}d{#}", e.g. 2d6 returns the total"""
@@ -12,3 +13,30 @@ def roll(dice):
     except Exception as e:
         print("Error: " + str(e))
         return "Something went wrong! Check your input."
+
+def tri_int(low, high, mode):
+    """Returns an integer in a triangular distribution."""
+    return floor(triangular(low, high, mode))
+
+def tri_sample(population, k, mode=0):
+    """
+    Mimics the functionality of random.sample() but with a triangular
+    distribution over the length of the sequence.
+
+    Mode defaults to 0, which favors lower indices.
+    """
+    psize = len(population)
+    if k > psize:
+        raise ValueError("k must be less than the number of items in population.")
+    if mode > psize:
+        raise ValueError("mode must be less than the number of items in population.")
+    indices_chosen = []
+    sample = []
+    for i in range(k):
+        while True:
+            choice = tri_int(0, psize, mode)
+            if choice not in indices_chosen:
+                break
+        indices_chosen.append(choice)
+        sample.append(population[choice])
+    return sample
